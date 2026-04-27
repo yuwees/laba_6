c19c55748c97743c1df1763b8fa98dbf13eb5ae8
Julia <yuliya.sandu26@gmail.com>
2026-04-27 08:39:19 +0200

    Update Inkltem.h

--- Inkltem.h
+++ Inkltem.h
@@ -1,1 +1,25 @@
-j
+#ifndef INKITEM_H
+#define INKITEM_H
+
+#include <QString>
+#include <QJsonObject>
+
+class InkItem
+{
+public:
+    QString name;        // название
+    QString description; // описание
+    QString color;       // цвет
+    double inkLevel;     // уровень чернил
+
+    InkItem();
+
+    bool isValid() const;
+
+    QJsonObject toJson() const;
+    static InkItem fromJson(const QJsonObject &obj);
+
+    static InkItem fromString(const QString &line);
+};
+
+#endif // INKITEM_H
